54fa2e5e4053847e332d7c033c103e4b34ec46fb
Artur Kruk <artur.kruk@intenso.pl>
2018-01-14 16:25:36 +0100

    Finish dialog

--- server/view/client/src/App.js
+++ server/view/client/src/App.js
@@ -1,19 +1,50 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import OrderDialog from './components/OrderDialog/OrderDialog';
 
+const orderPositions = {
+    "wares": [
+        {
+            "_id": "5a5a5b27b84a2453c8555772",
+            "nazwa": "Cegły-pustaki",
+            "min": 1,
+            "max": 10,
+            "magazyn": "5a59f586ac2c185720334d56",
+            "__v": 0
+        },
+        {
+            "_id": "5a5b66f3c5c433065cfb25ca",
+            "nazwa": "Deski hebanowe",
+            "min": 1,
+            "max": 100,
+            "magazyn": "5a59f586ac2c185720334d56"
+        }
+    ]
+};
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <OrderDialog
-          title='Dodaj pozycje zamówienia'
-          cancelText='Anuluj'
-          confirmText='Dodaj'
-        />
-      </div>
-    );
-  }
+
+    onConfirm = (data) => {
+        console.log(data);
+    }
+
+    onCancel = () => {
+
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <OrderDialog
+                    positions={orderPositions}
+                    title='Dodaj pozycje zamówienia'
+                    cancelText='Anuluj'
+                    confirmText='Dodaj'
+                    onConfirm={this.onConfirm}
+                />
+            </div>
+        );
+    }
 }
 
 export default App;
--- server/view/client/src/components/OrderDialog/OrderDialog.js
+++ server/view/client/src/components/OrderDialog/OrderDialog.js
@@ -3,11 +3,31 @@ import OrderDialogStyle from './OrderDialogStyle';
 import Button from '@atlaskit/button';
 import Modal from '@atlaskit/modal-dialog';
 import SingleSelect from '@atlaskit/single-select';
-import TextField from '@atlaskit/field-text';
+import TextField, { FieldTextStateless } from '@atlaskit/field-text';
 
 class OrderDialog extends Component {
 
+    state = {
+        selectedId: '',
+        amount: 0
+    }
+
+    onItemSelected = (selectedItem) => {
+        this.setState({selectedId: selectedItem.item.value})
+    }
+
+    onAmountChanged = (e) => {
+        this.setState({amount: Math.abs(e.target.value)})
+    }
+
     render() {
+
+        const items = this.props.positions.wares.map((ware) => {
+            return { content: ware.nazwa, value: ware._id }
+        });
+
+        const selectItems = [{ items: items }];
+
         return (
             <OrderDialogStyle>
                 <Modal heading={this.props.title}>
@@ -15,22 +35,31 @@ class OrderDialog extends Component {
                         <SingleSelect
                             label='Wybierz towar:'
                             appearance='subtle'
+                            items={selectItems}
+                            onSelected={this.onItemSelected}
                             shouldFitContainer
+                            required
                         />
 
-                        <TextField
+                        <FieldTextStateless
                             type='Number'
                             label='Podaj ilość [szt]:'
                             shouldFitContainer
+                            min={0}
+                            required
+                            value={this.state.amount}
+                            onChange={this.onAmountChanged}
                         />
                     </div>
                     <div style={{ marginTop: '20px', marginBottom: '20px', display: 'flex', justifyContent: 'flex-end' }}>
-                        <Button>
+                        <Button
+                            onClick={this.props.onCancel}>
                             {this.props.cancelText}
                         </Button>
                         <div style={{ width: '10px' }}/>
                         <Button
                             appearance='primary'
+                            onClick={() => this.props.onConfirm({ selectedId: this.state.selectedId, amount: this.state.amount})}
                         >
                             {this.props.confirmText}
                         </Button>
